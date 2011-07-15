3c7c3a4f76e37f743c6374e2e6217848a5ad9191
FriedRice <n.mclaughlin22@yahoo.com>
2011-07-15 10:57:25 -0400

    Added Foreign Key relationships to models

--- association/models.py
+++ association/models.py
@@ -1,10 +1,5 @@
 from django.db import models
 
-class Association(models.Model):
-    notes = models.CharField(max_length=200)
-    date = models.DateField(auto_now_add=True)
-    status = models.BooleanField()
-
 class GitTicket(models.Model):
     number = models.IntegerField(primary_key=True)
     gitType = models.CharField(max_length=20)
@@ -17,3 +12,10 @@ class ZenTicket(models.Model):
     zenType = models.CharField(max_length=20)
     priority = models.CharField(max_length=20)
     desc = models.TextField()
+
+class Association(models.Model):
+    git = models.ForeignKey(GitTicket)
+    zen = models.ForeignKey(ZenTicket)
+    notes = models.CharField(max_length=200)
+    date = models.DateField(auto_now_add=True)
+    status = models.BooleanField()
