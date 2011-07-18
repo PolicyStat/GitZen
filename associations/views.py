f2704fe8e53d5ec41eff8b0ccd884da0a51d6847
FriedRice <n.mclaughlin22@yahoo.com>
2011-07-18 12:49:23 -0400

    Added info pages for git tickets and added hyperlinks to the home page to reach the info pages.

--- associations/views.py
+++ associations/views.py
@@ -1,13 +1,21 @@
 from django.http import HttpResponse
 from github2.client import Github
 from github2.issues import *
+from zendesk import Zendesk
 
 github = Github(username='FriedRice', api_token='68c7180e60ad2354b9bc84792c6ba7ab')
+#zendesk = Zendesk() 
 
 def home(request):
     issues = github.issues.list('PolicyStat/PolicyStat', state='open')
-    output = '<br/>'.join([str(i.number) + '. ' + i.title for i in issues])
+    output = '<br/>'.join(['<a href="/as/git/%s/">%s.</a> %s' % (i.number, 
+                        i.number, i.title) for i in issues])
     return HttpResponse(output)
 
 def git(request, git_num):
-    pass
+    issue = github.issues.show('PolicyStat/PolicyStat', git_num)
+    output = '<br/>'.join(['%s: %s' % (k, v) for k, v in issue])
+    return HttpResponse(output)
+
+def zen(request, zen_num):
+   pass 
