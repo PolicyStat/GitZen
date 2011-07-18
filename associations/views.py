4134fef461c6a5c2c7d6dad3acb8475b15dc261c
FriedRice <n.mclaughlin22@yahoo.com>
2011-07-18 12:10:33 -0400

    Created base url structure and added a simple view of Github tickets.

--- associations/views.py
+++ associations/views.py
@@ -1,1 +1,13 @@
-# Create your views here.
+from django.http import HttpResponse
+from github2.client import Github
+from github2.issues import *
+
+github = Github(username='FriedRice', api_token='68c7180e60ad2354b9bc84792c6ba7ab')
+
+def home(request):
+    issues = github.issues.list('PolicyStat/PolicyStat', state='open')
+    output = '<br/>'.join([str(i.number) + '. ' + i.title for i in issues])
+    return HttpResponse(output)
+
+def git(request, git_num):
+    pass
--- urls.py
+++ urls.py
@@ -5,13 +5,8 @@ from django.conf.urls.defaults import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'GitZen.views.home', name='home'),
-    # url(r'^GitZen/', include('GitZen.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-)
+    (r'^as/$', 'associations.views.home'),
+    (r'^as/new/$', 'associations.views.new'),
+    (r'^as/git/(?P<git_num>\d+)/$', 'associations.views.git'),
+    (r'^as/zen/(?P<zen_num>\d+)/$', 'associations.views.zen'),
+) 
