21f3642049da7040f136b518a1622534b054c532
FriedRice <n.mclaughlin22@yahoo.com>
2011-07-18 12:09:32 -0400

    Deleted unnecessary storing of Git and Zen data in database model.

--- associations/models.py
+++ associations/models.py
@@ -1,32 +1,10 @@
 from django.db import models
 
-class GitTicket(models.Model):
-    number = models.IntegerField()
-    title = models.CharField(max_length=100)
-    gitType = models.CharField(max_length=20)
-    desc = models.TextField()
-
-    def __unicode__(self):
-        return str(self.number)
-
-
-class ZenTicket(models.Model):
-    incident = models.IntegerField()
-    name = models.CharField(max_length=40)
-    email = models.EmailField()
-    zenType = models.CharField(max_length=20)
-    priority = models.CharField(max_length=20)
-    desc = models.TextField()
-
-    def __unicode__(self):
-        return str(self.incident)
-
-
 class Association(models.Model):
-    git = models.ForeignKey(GitTicket)
-    zen = models.ForeignKey(ZenTicket)
+    git = models.IntegerField()
+    zen = models.IntegerField()
     notes = models.CharField(max_length=200)
-    date = models.DateField(auto_now_add=True)
+    date = models.DateTimeField(auto_now_add=True)
     status = models.BooleanField()
 
     def __unicode__(self):
