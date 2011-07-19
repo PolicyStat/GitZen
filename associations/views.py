a152af46777400600463a171b9a4a87b7f4b189c
FriedRice <n.mclaughlin22@yahoo.com>
2011-07-19 12:27:39 -0400

    Added Associations list and creator to the main page. The creator is not fully working.

--- associations/views.py
+++ associations/views.py
@@ -1,17 +1,39 @@
 from django.http import HttpResponse
-from django.template import Context, loader
 from django.shortcuts import render_to_response
+from django import forms
 from github2.client import Github
 from github2.issues import *
 from zendesk import Zendesk
+from associations.models import Association
 
 github = Github(username='FriedRice', api_token='68c7180e60ad2354b9bc84792c6ba7ab')
 repo = 'PolicyStat/PolicyStat'
-zendesk = Zendesk('https://policystat.zendesk.com', '', '') 
+zendesk = Zendesk('https://policystat.zendesk.com', '', '')
+
+class AssocForm(forms.Form):
+    gnum = forms.IntegerField()
+    znum = forms.IntegerField()
+    notes = forms.CharField(max_length=200)
 
 def home(request):
     issues = github.issues.list(repo, state='open')
-    return render_to_response('associations/home.html', {'issues': issues})
+    assocs = Association.objects.all().order_by('git')
+    opentickets = [i.number for i in github.issues.list(repo, state='open')]
+    if request.method == 'POST':
+        form = AssocForm(request.POST)
+        if form.is_valid():
+            a = Association(git=form.cleaned_data['gnum'],
+            zen=form.cleaned_data['znum'], notes=form.cleaned_data['notes'],
+            status=True)
+            a.save()
+
+            return HttpResponseRedirect('/as/')
+    else:
+        form = AssocForm()
+
+    return render_to_response('associations/home.html', {'issues': issues,
+                                'assocs': assocs, 'opentickets': opentickets, 
+                                'form':form,})
 
 def git(request, git_num):
     issue = github.issues.show(repo, git_num)
