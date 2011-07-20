8b8752db653936a89c08b260868d7e5459e0bf21
FriedRice <n.mclaughlin22@yahoo.com>
2011-07-20 10:51:00 -0400

    Formatted the data table used for Associations.

--- associations/views.py
+++ associations/views.py
@@ -9,7 +9,8 @@ from associations.models import Association
 
 github = Github(username='FriedRice', api_token='68c7180e60ad2354b9bc84792c6ba7ab')
 repo = 'PolicyStat/PolicyStat'
-zendesk = Zendesk('https://policystat.zendesk.com', '', '')
+zendesk = Zendesk('https://policystat.zendesk.com', 'wes@policystat.com',
+                    'l2us3apitokens')
 
 class AssocForm(forms.Form):
     gnum = forms.IntegerField()
@@ -20,6 +21,15 @@ def home(request):
     issues = github.issues.list(repo, state='open')
     assocs = Association.objects.all().order_by('git')
     opentickets = [i.number for i in github.issues.list(repo, state='open')]
+    dates = {}
+    gitLabels = {}
+    for i in github.issues.list(repo, state='open'):
+        dates[i.number] = i.updated_at
+        gitLabels[i.number] = i.labels
+    for i in github.issues.list(repo, state='closed'):
+        dates[i.number] = i.closed_at
+        gitLabels[i.number] = i.labels
+        
     if request.method == 'POST':
         form = AssocForm(request.POST)
         if form.is_valid():
@@ -32,7 +42,8 @@ def home(request):
         form = AssocForm()
 
     return render_to_response('associations/home.html', {'issues': issues,
-                                'assocs': assocs, 'opentickets': opentickets, 
+                                'assocs': assocs, 'opentickets': opentickets,
+                                'dates': dates, 'gitLabels': gitLabels, 
                                 'form':form,}, context_instance=
                                 RequestContext(request))
 
