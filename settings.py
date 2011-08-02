3292a07227a8b18fcef192c5f179e260a1b204e7
FriedRice <n.mclaughlin22@yahoo.com>
2011-08-02 11:09:59 -0400

    Adjusted settings file for dotcloud upload.

--- settings.py
+++ settings.py
@@ -1,4 +1,6 @@
-# Django settings for GitZen project.
+# Django settings for gitzen project.
+
+import os.path 
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -9,12 +11,14 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
+PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'GitZen',                      # Or path to database file if using sqlite3.
-        'USER': 'root',                      # Not used with sqlite3.
-        'PASSWORD': 'gipper22',                  # Not used with sqlite3.
+        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': os.path.join(PROJECT_ROOT, '..', 'gitzen.db'),                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
@@ -100,10 +104,10 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'GitZen.urls'
+ROOT_URLCONF = 'gitzen.urls'
 
 TEMPLATE_DIRS = (
-    "/home/nick/GitZen/",
+    os.path.join(PROJECT_ROOT, 'templates'),
 )
 
 INSTALLED_APPS = (
