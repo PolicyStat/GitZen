28ef855f390b44a55ba83f3af4e4f0df1bc27ddd
FriedRice <n.mclaughlin22@yahoo.com>
2011-08-04 10:46:27 -0400

    Slight change in settings to try to fix deployment issue.

--- gitzen/settings.py
+++ gitzen/settings.py
@@ -15,7 +15,7 @@ PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': os.path.join(PROJECT_ROOT, '..', 'gitzen.db'),                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
