25b6db362ac218bcc665ecc142f79f7efaf59de0
FriedRice <n.mclaughlin22@yahoo.com>
2012-06-05 12:35:10 -0400

    Made a couple more adjustments to prepare for deploying to heroku

--- settings.py
+++ settings.py
@@ -1,6 +1,7 @@
 # Django settings for gitzen project.
 
-import os.path 
+import os.path
+import dj_database_url
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -14,15 +15,8 @@ MANAGERS = ADMINS
 PROJECT_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.path.join(PROJECT_ROOT, '..', 'gitzen.db'),                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+    'default': dj_database_url.config(default='postgres://localhost')
     }
-}
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
