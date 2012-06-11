9105f3e3df4f077be1ac2506737bf232756d5827
FriedRice <n.mclaughlin22@yahoo.com>
2012-06-11 12:58:03 -0400

    Attempting to add South module for migrations

--- settings.py
+++ settings.py
@@ -106,7 +106,8 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'associations'
+    'associations',
+    'south'
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
