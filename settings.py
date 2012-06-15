ccd793e76d1e18eb91df24fe9f66287096e08ea3
FriedRice <n.mclaughlin22@yahoo.com>
2012-06-15 12:12:28 -0400

    Changed database settings to allow for deployment to heroku instead of local testing

--- settings.py
+++ settings.py
@@ -149,7 +149,7 @@ LOGGING = {
     }
 }
 
-#import dj_database_url
-#DATABASES = {
-#    'default': dj_database_url.config(default='postgres://localhost')
-#    }
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
