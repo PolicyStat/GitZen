04f60d3537427754c6fcd58070d5b9f173090ad6
FriedRice <n.mclaughlin22@yahoo.com>
2012-06-25 12:47:53 -0400

    gz-8: Removed files and settings no longer used because of the new user model.

--- settings.py
+++ settings.py
@@ -121,13 +121,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
-#AUTHENTICATION_BACKENDS = (
-#    'enhancement_tracking.auth_backends.GZUserModelBackend',
-#)
-
 AUTH_PROFILE_MODULE = 'enhancement_tracking.GZUserProfile'
-
-#CUSTOM_USER_MODEL = 'enhancement_tracking.GZUser'
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
