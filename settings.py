12ea8abd5cc7c743be6a483117cff85ebccabcf0
FriedRice <n.mclaughlin22@yahoo.com>
2012-06-27 12:28:29 -0400

    gz-9: Adjusted database settings for heroku.

--- settings.py
+++ settings.py
@@ -152,7 +152,7 @@ LOGGING = {
     }
 }
 
-#import dj_database_url
-#DATABASES = {
-#    'default': dj_database_url.config(default='postgres://localhost')
-#    }
+import dj_database_url
+DATABASES = {
+    'default': dj_database_url.config(default='postgres://localhost')
+    }
