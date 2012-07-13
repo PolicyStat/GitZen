b42cb0fde679ff462da3f7f71a8ddfd1c6c87f7f
FriedRice <n.mclaughlin22@yahoo.com>
2012-07-13 10:35:06 -0400

    gz-20: Fixed another function parameter error.

--- enhancement_tracking/views.py
+++ enhancement_tracking/views.py
@@ -351,7 +351,7 @@ def get_git_tickets(request, git_issue_numbers):
     return git_tickets
 
 def build_enhancement_data(zen_tickets, zen_user_reference, git_tickets,
-                           zen_fieldid, utc_offset, api_status):
+                           zen_fieldid, utc_offset):
     """Builds the enhancement tracking tables from the Zendesk and GitHub data.
     
     Parameters:
