1ee1110826f7e684ffdb568ab4b9f196da10e64d
FriedRice <n.mclaughlin22@yahoo.com>
2012-07-23 10:55:57 -0400

    gz-22: Setup a new url structure in the urls.py file; now need to implement across the application.

--- urls.py
+++ urls.py
@@ -7,8 +7,13 @@ from django.conf.urls.defaults import patterns, include, url
 urlpatterns = patterns('enhancement_tracking.views',
     url(r'^$', 'user_login_form_handler', name='login'),
     url(r'^create/$', 'user_creation_form_handler', name='user_creation'),
-    url(r'^home/$', 'home', name='home'),
-    url(r'^change/$', 'change_form_handler', name='change'),
-    url(r'^git_confirm/$', 'git_oauth_confirm', name='git_confirm'),
-    url(r'^confirm/(?P<con_num>\d+)/$', 'confirm', name='confirm'),
-) 
+    url(r'^confirm_user_creation/$', 'confirm_user_creation', 
+        name='confirm_user_creation'),
+    url(r'^confirm_git_oauth/$', 'confirm_git_oauth', name='confirm_git_oauth'),
+    url(r'^home/$', 'check_authentication_frontend',
+        kwargs={'view_function': 'home'}, name='home'),
+    url(r'^change/$', 'check_authentication_frontend',
+        kwargs={'view_function': 'change_form_handler'}, name='change'),
+    url(r'^confirm_changes/$', 'check_authentication_frontend',
+        kwargs={'view_function': 'confirm_changes'}, name='confirm_changes')
+)
