581bbc8e3ec5cf791ee21f5a37e9a14f44d4c208
FriedRice <n.mclaughlin22@yahoo.com>
2012-07-26 12:33:40 -0400

    gz-14: Addressed an indention issue and eliminated some unnecessary code in the change profile form.

--- enhancement_tracking/forms.py
+++ enhancement_tracking/forms.py
@@ -26,11 +26,6 @@ class ProfileChangeForm(ModelForm):
     have their intial values set at the field's value for the current user's
     profile. Changing the git_token and zen_token are handled in seperate
     forms."""
-    def __init__(self, *args, **kwargs):
-        super(ProfileChangeForm, self).__init__(*args, **kwargs)
-        for key, value in self.fields.items():
-            self.fields[key].initial = getattr(self.instance, key)
-
     class Meta:
         model = GZUserProfile
         exclude = ('user', 'git_token', 'zen_token')
