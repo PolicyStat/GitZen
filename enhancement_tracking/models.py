f76f8f96b5036d7ee9063756ee590e61e8397bf9
FriedRice <n.mclaughlin22@yahoo.com>
2012-08-02 09:46:00 -0400

    gz-24: Reset migrations to properly run them on heroku.

--- enhancement_tracking/models.py
+++ enhancement_tracking/models.py
@@ -2,6 +2,11 @@ from django.db import models
 from django.contrib.auth.models import User
 from encryption import EncryptedCharField
 
+VIEW_TYPE_CHOICES = (
+    ('GIT', 'GitHub'),
+    ('ZEN', 'Zendesk')
+)
+
 class GZUserProfile(models.Model):
     user = models.ForeignKey(User, unique=True)
     git_token = EncryptedCharField(max_length=75)
@@ -20,6 +25,8 @@ class GZUserProfile(models.Model):
                                       Association Field ID')
     utc_offset = models.IntegerField(null=True, 
                                      verbose_name='Time Zone (UTC Offset)')
-    
+    view_type = models.CharField(max_length=3, choices=VIEW_TYPE_CHOICES,
+                                 default='ZEN', verbose_name='View Type')
+
     def __str__(self):
         return "%s's profile" % self.user
