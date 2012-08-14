4b4a2ac4f0c9f16b6cdc4287e8f8cdc679ef9d4d
winhamwr <winhamwr@gmail.com>
2012-08-14 18:04:54 -0400

    Trying some import hackery to make this project structure work.

--- manage.py
+++ manage.py
@@ -2,13 +2,13 @@
 from django.core.management import execute_manager
 import imp
 try:
-    imp.find_module('settings') # Assumed to be in the same directory.
+    m_file, m_pathname, m_description = imp.find_module('settings', ['gitzen'])
 except ImportError:
     import sys
     sys.stderr.write("Error: Can't find the file 'settings.py' in the directory containing %r. It appears you've customized things.\nYou'll have to run django-admin.py, passing it your settings module.\n" % __file__)
     sys.exit(1)
 
-import settings
+settings = imp.load_module('settings', m_file, m_pathname, m_description)
 
 if __name__ == "__main__":
     execute_manager(settings)
